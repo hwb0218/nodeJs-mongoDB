6d561ab78eb0cc4f9a73366fb4103ee6a310b463
WOOBEEN <hwb0218@gmail.com>
2021-10-01 22:04:09 +0900

    feat: store 라우터 multer 미들웨어 등록

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -2,10 +2,11 @@ const express = require("express");
 const EmployeeController = require("../controllers/EmployeeController");
 
 const router = express.Router();
+const upload = require("../middleware/multer");
 
 router.get("/", EmployeeController.index);
 router.post("/show", EmployeeController.show);
-router.post("/store", EmployeeController.store);
+router.post("/store", upload.single("avatar"), EmployeeController.store);
 router.post("/update", EmployeeController.update);
 router.post("/destory", EmployeeController.destory);
 
